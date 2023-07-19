18e248ca9aaddc754023024be1640f13649af91d
Mike Dark <agyemankojo47@gmail.com>
2023-07-19 09:59:10 +0000

    Create js array for list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import './style.css';
 
-// const array = [
-//   {
-//     description: 'To do list',
-//     completed: true,
-//     index: '1',
-//   },
-//   {
-//     description: 'Wash utensils',
-//     completed: true,
-//     index: '2',
-//   },
-//   {
-//     description: 'Wash movies',
-//     completed: true,
-//     index: '3',
-//   },
-// ];
+const listArray = [
+  {
+    description: 'Go on holidays',
+    completed: true,
+    index: '1',
+  },
+  {
+    description: 'Wash utensils',
+    completed: true,
+    index: '2',
+  },
+  {
+    description: 'Wash movies',
+    completed: true,
+    index: '3',
+  },
+];
+
+const taskcontainer = document.querySelector('tasklist');
+taskcontainer.innerHTML = '';
+
+const displayList = () => {
+  listArray.forEach((list) => {
+    const li = document.createElement('li');
+    li.classList.add('newlist');
+    li.innerHTML = ~=`
+    `
+
+  })
+};
