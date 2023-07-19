2dad0863ecffa6035c953216f82b7e6bc840f19f
Mike Dark <agyemankojo47@gmail.com>
2023-07-19 12:37:58 +0000

    Add style to list heading

--- src/index.js
+++ src/index.js
@@ -3,30 +3,44 @@ import './style.css';
 const listArray = [
   {
     description: 'Go on holidays',
-    completed: true,
+    completed: false,
     index: '1',
   },
   {
     description: 'Wash utensils',
-    completed: true,
+    completed: false,
     index: '2',
   },
   {
-    description: 'Wash movies',
-    completed: true,
+    description: 'Watch movies',
+    completed: false,
+    index: '3',
+  }, {
+    description: 'Pray',
+    completed: false,
     index: '3',
   },
 ];
 
-const taskcontainer = document.querySelector('tasklist');
+const taskcontainer = document.querySelector('.tasklist');
 taskcontainer.innerHTML = '';
 
 const displayList = () => {
   listArray.forEach((list) => {
     const li = document.createElement('li');
     li.classList.add('newlist');
-    li.innerHTML = ~=`
-    `
-
-  })
+    li.innerHTML = `
+    <ul class="Tasklist">
+    <li>
+     <input type="checkbox" name="checkbox" id="check" />${list.description}<span
+      class="material-symbols-rounded"
+     >
+      delete
+     </span>
+    </li>
+   </ul>`;
+    taskcontainer.appendChild(li);
+  });
 };
+
+displayList();
