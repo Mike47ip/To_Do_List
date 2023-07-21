2c7bde4df942764e76f187d6f21c5cd70667dff9
Mike Dark <agyemankojo47@gmail.com>
2023-07-21 22:11:31 +0000

    Write Javascript to add functioanality to Page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-/* eslint-disable no-use-before-define */
 import './style.css';
+
+// Function to retrieve the task list from local storage
+function getTaskListFromLocalStorage() {
+  const taskListString = localStorage.getItem('taskList');
+  return taskListString ? JSON.parse(taskListString) : [];
+}
+
+// Function to save the task list to local storage
+function saveTaskListToLocalStorage(taskList) {
+  localStorage.setItem('taskList', JSON.stringify(taskList));
+}
+
+// Function to update the text of a task
+function updateTaskText(index, newText, isChecked) {
+  const taskList = getTaskListFromLocalStorage();
+  const taskToEdit = taskList.find((task) => task.index === index);
+
+  if (!taskToEdit) {
+    return;
+  }
+
+  taskToEdit.text = newText;
+  taskToEdit.checked = isChecked || false; // Save the checkbox state in the task object
+  saveTaskListToLocalStorage(taskList);
+}
 
 // Function to add a new task to the list
 function addTask() {
@@ -15,6 +39,7 @@ function addTask() {
   const newTask = {
     text: newTaskText,
     index: taskList.length + 1,
+    checked: false, // Set the checkbox state to false (unchecked) for new tasks
   };
 
   taskList.push(newTask);
@@ -22,6 +47,22 @@ function addTask() {
 
   newTaskInput.value = '';
   displayTasks(taskList);
+}
+
+// Function to handle key press events on the input field
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+}
+
+// New Changes: Add function to find parent li element based on the clicked delete icon
+function findParentListItem(index) {
+  const customDeleteIcon = document.getElementById(`deleteBtn${index}`);
+  if (!customDeleteIcon) {
+    return null;
+  }
+  return customDeleteIcon.closest('li');
 }
 
 // Function to delete a task from the list
@@ -42,59 +83,83 @@ function deleteTask(index) {
 }
 
 function displayTasks(taskList) {
-  const taskListElement = document.getElementById('taskList');
+  const taskListElement = document.querySelector('.taskList');
+  if (!taskListElement) {
+    return; // Exit the function if the task list element is not found
+  }
   taskListElement.innerHTML = '';
 
   taskList.forEach((task) => {
     const listItem = document.createElement('li');
-    listItem.textContent = task.text;
-    listItem.setAttribute('contenteditable', 'true'); // Add the contenteditable attribute
 
-    listItem.addEventListener('input', () => {
-      updateTaskText(task.index, listItem.textContent.trim());
+    const checkbox = document.createElement('input');
+    checkbox.classList.add('checkbox');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.checked;
+
+    const taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-container');
+
+    const taskText = document.createElement('span');
+    taskText.classList.add('taskText');
+    taskText.textContent = task.text;
+    taskText.setAttribute('contenteditable', 'true');
+
+    const customDeleteIcon = document.createElement('span');
+    customDeleteIcon.innerHTML = `<span id="deleteBtn${task.index}" class="material-symbols-outlined"> delete </span>`;
+    customDeleteIcon.classList.add('custom-delete-icon'); // Add your custom class for the icon
+
+    taskContainer.appendChild(checkbox);
+    taskContainer.appendChild(taskText);
+    taskContainer.appendChild(customDeleteIcon);
+
+    listItem.appendChild(taskContainer);
+
+    // Add an event listener to the customDeleteIcon to delete the task when clicked
+    customDeleteIcon.addEventListener('click', () => {
+      const parentListItem = findParentListItem(task.index);
+      if (parentListItem) {
+        parentListItem.remove();
+        deleteTask(task.index); // Call deleteTask function when the custom icon is clicked
+      }
     });
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.onclick = () => deleteTask(task.index);
+    // Add an event listener to the checkbox to update the "checked" attribute
+    checkbox.addEventListener('change', () => {
+      updateTaskText(task.index, task.text, checkbox.checked);
+    });
 
-    listItem.appendChild(deleteButton);
     taskListElement.appendChild(listItem);
   });
 }
 
-// Function to save the task list to local storage
-function saveTaskListToLocalStorage(taskList) {
-  localStorage.setItem('taskList', JSON.stringify(taskList));
-}
+// Function to clear all completed tasks (checkbox checked)
+function clearCompletedTasks() {
+  const taskList = getTaskListFromLocalStorage();
 
-// Function to retrieve the task list from local storage
-function getTaskListFromLocalStorage() {
-  const taskListString = localStorage.getItem('taskList');
-  return taskListString ? JSON.parse(taskListString) : [];
+  // Loop through the taskList in reverse to avoid index shifting issues
+  for (let i = taskList.length - 1; i >= 0; i -= 1) {
+    if (taskList[i].checked) {
+      taskList.splice(i, 1); // Remove the completed task from the taskList
+    }
+  }
+
+  // Save the updated task list to local storage
+  saveTaskListToLocalStorage(taskList);
+
+  // Display the updated task list
+  displayTasks(taskList);
 }
 
 // Load the initial tasks on page load
-document.addEventListener('DOMContentLoaded', () => {
-  const addButton = document.getElementById('addButton');
-  addButton.innerText = 'Add Task';
-  addButton.addEventListener('click', addTask);
+const addButton = document.getElementById('keyboard_return');
+addButton.addEventListener('click', addTask);
 
-  const initialTaskList = getTaskListFromLocalStorage();
-  displayTasks(initialTaskList);
-});
+const clearButton = document.getElementById('clearCompletedButton'); // Use the "id" attribute
+clearButton.addEventListener('click', clearCompletedTasks);
 
-// Function to update the text of a task
-function updateTaskText(index, newText) {
-  if (newText !== '') {
-    const taskList = getTaskListFromLocalStorage();
-    const taskToEdit = taskList.find((task) => task.index === index);
+const initialTaskList = getTaskListFromLocalStorage();
+displayTasks(initialTaskList);
 
-    if (!taskToEdit) {
-      return;
-    }
-
-    taskToEdit.text = newText;
-    saveTaskListToLocalStorage(taskList);
-  }
-}
+const newTaskInput = document.getElementById('inputPlaceholder');
+newTaskInput.addEventListener('keypress', handleKeyPress);
